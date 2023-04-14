d6868fbed9cbdf9ef6395d6b86ae0c3c06f0c4a0
pablo martin sosa <pablo25.1996@gmail.com>
2023-04-14 16:40:27 -0300

    agrego carpeta fotos y funcionalidad al carrusel
    agrego 3 carpetas con 3 fotos cada una, tambien le agrego funcionalidad al carrusel donde cambia las fotos dependiendo del div elegido, mas una animacion de salida y entrada

--- script.js
+++ script.js
@@ -3,18 +3,51 @@
 document.addEventListener('DOMContentLoaded', function() {
     var divs = document.getElementsByClassName('divSombreado');
     
-    
+    let frase1="Fotografa experta en fiestas de quince"
+    let frase2="Fotrografo especializado en bodas y eventos formales"
+    let frase3="Fotografa de exteriores y zonas urbanas"
+    let frase4="Fotografa de eventos deportivos y campeonatos"
+
+    let arrayFrases=[frase1,frase2,frase3,frase4]
+    console.log(arrayFrases);
     for (var i = 0; i < divs.length; i++) {
       divs[i].addEventListener('click', function() {
         // Removemos la clase 'sombreado' de todos los divs
         for (var j = 0; j < divs.length; j++) {
           divs[j].classList.remove('sombreado');
           divs[j].classList.remove('bgGrey');
+
         }
+        // Identifico que div llamo el evento
+        numero = this.id.split("")[(this.id.split("").length)-1]
         
+        p= $("#fraseFotografo")
+        //p.text(arrayFrases[numero]).fadeIn(200)
+
+
+        p.fadeOut(500, function() {
+            $(this).text(arrayFrases[numero]).fadeIn(500);
+        });
+
         // Agregamos la clase 'sombreado' al div clickeado
         this.classList.add('sombreado');
-        this.getElementsByClassName("card-text").classList.add("sombreado")
+        
+        //Cambiare las fotos
+        let imagenesCarrusel= $(".carousel-item img")
+        let fotosCarrusel1=["imagen1.jpg", "imagen2.jpg", "imagen3.jpg"]
+        
+        //let arrayFotosCarrusel=[]
+        console.log(imagenesCarrusel);
+        
+        imagenesCarrusel.fadeOut(1000, function() {
+            for (var k = 0; k < imagenesCarrusel.length; k++) {
+                let nuevoPath= "fotos/"+"carrusel"+numero+"/"+fotosCarrusel1[k]
+                imagenesCarrusel.eq(k).attr("src",nuevoPath)
+              }
+            
+            // Muestra las nuevas imágenes de forma animada
+            imagenesCarrusel.fadeIn(1000);
+          });
         
         
       });
